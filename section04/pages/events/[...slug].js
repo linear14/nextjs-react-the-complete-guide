25441fa538d97ee388b08b2e30efcb6456d41e80
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2022-03-06 17:06:44 +0900

    70. Setting Up The Main Pages

---
+++ section04/pages/events/[...slug].js
@@ -0,0 +1,3 @@
+export default function FilteredEventPage() {
+  return <div>FilteredEventPage</div>;
+}
---
+++ section04/pages/events/[id].js
@@ -0,0 +1,3 @@
+export default function SpecificEventPage() {
+  return <div>SpecificEventPage</div>;
+}
