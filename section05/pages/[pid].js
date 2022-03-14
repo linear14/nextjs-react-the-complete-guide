d34482c1bbd436882e2f20b07bf4d258c8957bc4
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2022-03-14 14:11:53 +0900

    101. Loading Paths Dynamically

--- section05/pages/[pid].js
+++ section05/pages/[pid].js
@@ -19,14 +19,19 @@ function ProductDetailPage(props) {
   );
 }
 
-export async function getStaticProps(context) {
-  const { params } = context;
-  const { pid } = params;
-
+async function getData() {
   const pathName = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(pathName);
   const data = JSON.parse(jsonData);
 
+  return data;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const { pid } = params;
+
+  const data = await getData();
   const product = data.products.find((item) => item.id === pid);
 
   return {
@@ -37,9 +42,13 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+  const ids = data.products.map((item) => item.id);
+  const pathWithParams = ids.map((id) => ({ params: { pid: `${id}` } }));
+
   return {
-    paths: [{ params: { pid: "p1" } }],
-    fallback: true,
+    paths: pathWithParams,
+    fallback: false,
   };
 }
 
