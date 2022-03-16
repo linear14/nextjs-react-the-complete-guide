b0c11fae9dbc671bf858c644897caba998a7c98d
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2022-03-16 16:00:11 +0900

    141 ~ 142. Make FE Form + Parsing The Incoming Request

--- section08/pages/api/feedback.js
+++ section08/pages/api/feedback.js
@@ -1,5 +1,27 @@
-function handler(req, res) {
-  res.status(200).json({ message: "This works!" });
+import fs from "fs/promises";
+import path from "path";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, feedbackText } = req.body;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email,
+      text: feedbackText,
+    };
+
+    // store
+    const filePath = path.join(process.cwd(), "data", "feedback.json");
+    const fileData = await fs.readFile(filePath);
+    const data = JSON.parse(fileData);
+    data.push(newFeedback);
+    await fs.writeFile(filePath, JSON.stringify(data));
+
+    res.status(201).json({ message: "Success!", feedback: newFeedback });
+  } else {
+    res.status(200).json({ message: "This works!" });
+  }
 }
 
 export default handler;
