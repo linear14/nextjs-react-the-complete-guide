0b8ce8e2ac513d3edb393d087061a1f5be960f3c
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2022-03-18 23:10:03 +0900

    finished section10

--- section10/components/input/newsletter-registration.js
+++ section10/components/input/newsletter-registration.js
@@ -1,29 +1,51 @@
-import { useRef } from "react";
+import { useContext, useRef } from "react";
+import NotificationContext from "../../store/notification-context";
 import classes from "./newsletter-registration.module.css";
 
 function NewsletterRegistration() {
   const inputRef = useRef();
+  const notiCtx = useContext(NotificationContext);
 
   async function registrationHandler(event) {
     event.preventDefault();
 
-    // fetch user input (state or refs)
-    // optional: validate input
-    // send valid data to API
-    const reqBody = {
-      email: inputRef.current.value,
-    };
-
-    const response = await fetch("/api/newsletter", {
-      method: "POST",
-      body: JSON.stringify(reqBody),
-      headers: {
-        "Content-Type": "application/json",
-      },
+    notiCtx.showNotification({
+      title: "서버 요청 중...",
+      message: "서버로 데이터를 요청하고 있습니다.",
+      status: "pending",
     });
 
-    const data = await response.json();
-    console.log(data);
+    try {
+      const reqBody = {
+        email: inputRef.current.value,
+      };
+
+      const response = await fetch("/api/newsletter", {
+        method: "POST",
+        body: JSON.stringify(reqBody),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message);
+      }
+
+      notiCtx.showNotification({
+        title: "등록 완료",
+        message: `${data.email}님의 구독 신청이 완료되었습니다.`,
+        status: "success",
+      });
+    } catch (e) {
+      notiCtx.showNotification({
+        title: "에러 발생",
+        message: e.message || "알 수 없는 오류가 발생했습니다.",
+        status: "error",
+      });
+    }
   }
 
   return (
